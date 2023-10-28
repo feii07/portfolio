0d3bad441c7e8da6f559286da29588ca6d244485
Feii07 <christy.ferani@gmail.com>
2023-10-29 00:33:46 +0700

    image

--- src/component/content/project/Project.jsx
+++ src/component/content/project/Project.jsx
@@ -7,119 +7,119 @@ import { faUikit } from "@fortawesome/free-brands-svg-icons";
 const projects = [
     {
         type: 1,
-        image: "/img/project/web/chewon.png",
+        image: "./porfolio/img/project/web/chewon.png",
         title: "Gistex Chewon",
         description: "is a company website of Gistex Chewon to introduce the company to customer.",
         code: "Heroku, Wordpress"
     },
     {
         type: 1,
-        image: "/img/project/web/trackingyogya.png",
+        image: "./porfolio/img/project/web/trackingyogya.png",
         title: "Yogya Tracking Feature",
         description: "is a feature for track customer order of Yogya Group FnB.",
         code: "Laravel, PostgreSql"
     },
     {
         type: 1,
-        image: "/img/project/web/yukbisayukweb.png",
+        image: "./porfolio/img/project/web/yukbisayukweb.png",
         title: "YukBisaYuk",
         description: "is a website that aims to help people by donating and participation in petitions",
         code: "Laravel , MySQL"
     },
     {
         type: 1,
-        image: "/img/project/web/restaurant.jpeg",
+        image: "./porfolio/img/project/web/restaurant.jpeg",
         title: "RestoranOKE",
         description: "This website has a great tools for customer, cashier, waiter, chef and HR for dine-in restaurant management.",
         code: "Laravel , MySQL"
     },
     {
         type: 1,
-        image: "/img/project/web/perpusMantap.jpeg",
+        image: "./porfolio/img/project/web/perpusMantap.jpeg",
         title: "Perpustakaan Mantap",
         description: "is a program that help at library management.",
         code: "Laravel , MongoDB"
     },
     {
         type: 1,
-        image: "/img/project/web/myinspiration.png",
+        image: "./porfolio/img/project/web/myinspiration.png",
         title: "My Inspiration",
         description: "is a website that inspires you with quotes and the backstory about famous people in the world.",
         code: "Laravel , PostgreSQL , Heroku Cloud"
     },
     {
         type: 1,
-        image: "/img/project/web/latravel.png",
+        image: "./porfolio/img/project/web/latravel.png",
         title: "LaTravel",
         description: "is a website that provide an information to help visitors decide where they want to go.",
         code: "Laravel , MySQL"
     },
     {
         type: 1,
-        image: "/img/project/web/dengerinaja.png",
+        image: "./porfolio/img/project/web/dengerinaja.png",
         title: "DengerinAja",
         description: "is a simple music website to list a music, add new music and so on.",
         code: "Laravel , VueJS , MySQL"
     },
     {
         type: 1,
-        image: "/img/project/web/baboonlibrary.png",
+        image: "./porfolio/img/project/web/baboonlibrary.png",
         title: "Babon Library",
         description: "is a library website for read a book and share a book.",
         code: "HTML , PHP , mySQL"
     },
     {
         type: 2,
-        image: "/img/project/android/kickshaw app.png",
+        image: "./porfolio/img/project/android/kickshaw app.png",
         title: "Kickshaw",
         description: "is a Android app that contains a lot of delicious recipe.",
         code: "Kotlin , Spoonacular API , Android Studio"
     },
     {
         type: 3,
-        image: "/img/project/ui/bioporang.png",
+        image: "./porfolio/img/project/ui/bioporang.png",
         title: "BIOPORANG",
         description: "is a mobile app that gives information about Porang.",
         code: "Figma"
     },
     {
         type: 3,
-        image: "/img/project/ui/funo.png",
+        image: "./porfolio/img/project/ui/funo.png",
         title: "Funo",
         description: "is a mobile app that allow you to book transportation online.",
         code: "Figma"
     },
     {
         type: 3,
-        image: "/img/project/ui/youthspace.png",
+        image: "./porfolio/img/project/ui/youthspace.png",
         title: "Youthspace",
         description: "is a cool mobile app for youth.",
         code: "Figma"
     },
     {
         type: 3,
-        image: "/img/project/ui/tunggalkreatifindo.png",
+        image: "./porfolio/img/project/ui/tunggalkreatifindo.png",
         title: "CV Tunggal Kreatifindo",
         description: "is a app for easily connect customer and CV Tunggal Kreatifindo company.",
         code: "Figma"
     },
     {
         type: 3,
-        image: "/img/project/ui/yukbisayuk.png",
+        image: "./porfolio/img/project/ui/yukbisayuk.png",
         title: "YukBisaYuk",
         description: "is a web design that aims to help people by donating and participation in petitions.",
         code: "Figma"
     },
     {
         type: 3,
-        image: "/img/project/ui/restoran.png",
+        image: "./porfolio/img/project/ui/restoran.png",
         title: "RestoranOKE",
         description: "is a web design for dine-in restaurant management.",
         code: "Figma"
     },
     {
         type: 3,
-        image: "/img/project/ui/lorem.png",
+        image: "./porfolio/img/project/ui/lorem.png",
         title: "Lorem",
         description: "is a web design that sells electronic stuff.",
         code: "Figma"
