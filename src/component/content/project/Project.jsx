0d690f4ef7828f8b6c5c4b5e441b294bd0277ec1
unknown <christy.ferani@gmail.com>
2023-10-29 17:05:27 +0700

    link to from card

--- src/component/content/project/Project.jsx
+++ src/component/content/project/Project.jsx
@@ -7,122 +7,139 @@ import { faUikit } from "@fortawesome/free-brands-svg-icons";
 const projects = [
     {
         type: 1,
-        image: "./portfolio/img/project/web/chewon.PNG",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/chewon.PNG",
         title: "Gistex Chewon",
         description: "is a company website of Gistex Chewon to introduce the company to customer.",
-        code: "Heroku, Wordpress"
+        code: "Heroku, Wordpress",
+        link: "https://gistexchewon.com/"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/trackingyogya.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/trackingyogya.PNG",
         title: "Yogya Tracking Feature",
         description: "is a feature for track customer order of Yogya Group FnB.",
-        code: "Laravel, PostgreSql"
+        code: "Laravel, PostgreSql",
+        link: "https://food.yogyagroup.com/tracking"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/yukbisayukweb.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/yukbisayukweb.PNG",
         title: "YukBisaYuk",
         description: "is a website that aims to help people by donating and participation in petitions",
-        code: "Laravel , MySQL"
+        code: "Laravel , MySQL",
+        link: "https://github.com/AlbertusKevin/YukBisaYuk"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/restaurant.jpeg",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/restaurant.jpeg",
         title: "RestoranOKE",
         description: "This website has a great tools for customer, cashier, waiter, chef and HR for dine-in restaurant management.",
-        code: "Laravel , MySQL"
+        code: "Laravel , MySQL",
+        link: "https://github.com/AlbertusKevin/YukBisaYuk"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/perpusMantap.jpeg",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/perpusMantap.jpeg",
         title: "Perpustakaan Mantap",
         description: "is a program that help at library management.",
-        code: "Laravel , MongoDB"
+        code: "Laravel , MongoDB",
+        link: "https://github.com/mikhaelAPG/PerpustakaanMantap"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/myinspiration.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/myinspiration.PNG",
         title: "My Inspiration",
         description: "is a website that inspires you with quotes and the backstory about famous people in the world.",
-        code: "Laravel , PostgreSQL , Heroku Cloud"
+        code: "Laravel , PostgreSQL , Heroku Cloud",
+        link: "https://myinspiration.herokuapp.com"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/latravel.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/latravel.PNG",
         title: "LaTravel",
         description: "is a website that provide an information to help visitors decide where they want to go.",
-        code: "Laravel , MySQL"
+        code: "Laravel , MySQL",
+        link: "https://gitlab.com/VinAlbert/la-travel"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/dengerinaja.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/dengerinaja.PNG",
         title: "DengerinAja",
         description: "is a simple music website to list a music, add new music and so on.",
-        code: "Laravel , VueJS , MySQL"
+        code: "Laravel , VueJS , MySQL",
+        link: "https://github.com/feii07/dengerinaja"
     },
     {
         type: 1,
-        image: "./portfolio/img/project/web/baboonlibrary.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/web/baboonlibrary.PNG",
         title: "Babon Library",
         description: "is a library website for read a book and share a book.",
-        code: "HTML , PHP , mySQL"
+        code: "HTML , PHP , mySQL",
+        link: "https://drive.google.com/file/d/1hneU75KF16k1pxyjhLl-lzSgfVo23Rf7/view?usp=sharing"
     },
     {
         type: 2,
-        image: "./portfolio/img/project/android/kickshaw app.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/android/kickshaw app.PNG",
         title: "Kickshaw",
         description: "is a Android app that contains a lot of delicious recipe.",
-        code: "Kotlin , Spoonacular API , Android Studio"
+        code: "Kotlin , Spoonacular API , Android Studio",
+        link: "https://github.com/beatfraps/Recipe"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/bioporang.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/bioporang.PNG",
         title: "BIOPORANG",
         description: "is a mobile app that gives information about Porang.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/ptr4buEM2RcQo22jTM8UaA/Bio-Porang?scaling=scale-down"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/funo.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/funo.PNG",
         title: "Funo",
         description: "is a mobile app that allow you to book transportation online.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/toroRSvUtuebVYGl6Nfy1x/Funo-Mobile-Prototype?node-id=0%3A1"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/youthspace.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/youthspace.PNG",
         title: "Youthspace",
         description: "is a cool mobile app for youth.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/KY2P5ym06HOzzwjtntObgs/Youthspace-Copy?type=design&node-id=0-1&t=yng5MxeAAW06SGOM-0&scaling=scale-down&page-id=0%3A1&prev-org-id=external-teams"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/tunggalkreatifindo.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/tunggalkreatifindo.PNG",
         title: "CV Tunggal Kreatifindo",
         description: "is a app for easily connect customer and CV Tunggal Kreatifindo company.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/uSdHLDtDQjxk8yD4x3JrbQ/Cv-Tunggal-Kreatifindo?node-id=0%3A1"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/yukbisayuk.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/yukbisayuk.PNG",
         title: "YukBisaYuk",
         description: "is a web design that aims to help people by donating and participation in petitions.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/Z20WAbZ14SXoRwQ9ulB7GP/Alur?node-id=0%3A1&scaling=min-zoom/"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/restoran.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/restoran.PNG",
         title: "RestoranOKE",
         description: "is a web design for dine-in restaurant management.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/cygBKSImYhWrtA3WuE7Ud8/Restoran-View?scaling=min-zoom"
     },
     {
         type: 3,
-        image: "./portfolio/img/project/ui/lorem.png",
+        image: "https://raw.githubusercontent.com/feii07/portfolio/master/public/img/project/ui/lorem.PNG",
         title: "Lorem",
         description: "is a web design that sells electronic stuff.",
-        code: "Figma"
+        code: "Figma",
+        link: "https://www.figma.com/proto/zkBmgCkEARRTpMa7fTJxaF/Lorem?scaling=min-zoom"
     },
 ];
 
@@ -191,7 +208,7 @@ function cardProject(item, key) {
             <div className="card-project">
                 <div className="content">
                     <img className="card-img" src={item.image} />
-                    <h6>{item.title}</h6>
+                    <a href={item.link}><h6>{item.title}</h6></a>
                     <div className="hover_content">
                         <p>{item.description}</p>
                     </div>
